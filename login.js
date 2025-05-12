3141aa6236366cf52d30140ea1a949b1c07ecc54
Nevzudin Buzađija <nevzudinbuzadija@Nevzudins-MacBook-Air.local>
2025-05-13 00:46:09 +0200

    main page

---
+++ login.js
@@ -0,0 +1,9 @@
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  if (username !== "" && password !== "") {
+    window.location.href = "index.html";
+  }
+});
